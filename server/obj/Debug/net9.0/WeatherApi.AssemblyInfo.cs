0b719c9abe25357e4cfc1bc681c33523bf6bddfc
togilny <togilny@gmail.com>
2025-08-15 21:32:56 +0100

    Update launch configuration for WeatherApi and add backend-only option

--- server/obj/Debug/net9.0/WeatherApi.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/WeatherApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e3a964c33677480f43777e45758c6b87c3b4e2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
